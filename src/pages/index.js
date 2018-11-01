7f346cb371472417efd63c38b809d9f5a9c36a38
Ernesto Guimerans <ernestog@propertyvista.com>
2018-11-01 15:59:42 -0400

    cc

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,22 @@ import Layout from "../components/layout"
 
 export default ({ data }) => {
   console.log(data)
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state
+      })
+    })
+      .then(() => navigateTo(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
+
   return (
     <Layout>
       <div>
@@ -48,20 +64,35 @@ export default ({ data }) => {
       </div>
       <div>
         <h3>Testin form</h3>
-        <form name="sms" method="POST" data-netlify="true">
-          <p>
-            <label>Your Name: <input type="text" name="name" /></label>
+        <div>
+        <h1>Contact</h1>
+        <form
+          name="contact"
+          method="post"
+          action="/about/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out:{" "}
+              <input name="bot-field"/>
+            </label>
           </p>
           <p>
-            <label>Your Phone: <input type="phone" name="phone" /></label>
-            </p>
-          <p>
-            <label>Your message: <textarea name="message"></textarea></label>
+            <label>
+              Your name:<br />
+              <input type="text" name="name" />
+            </label>
           </p>
           <p>
             <button type="submit">Send</button>
           </p>
         </form>
+      </div>
       </div>
     </Layout>
   )
