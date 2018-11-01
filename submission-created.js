2dbc77d118bdd68ec2e45e04023c42390c13cbba
Ernesto Guimerans <ernestog@propertyvista.com>
2018-11-01 17:57:15 -0400

    cc

--- submission-created.js
+++ submission-created.js
@@ -1,15 +1,42 @@
-exports.handler = function(event, context, callback) {
+exports.handler = async (event, context) => {
+  console.log('hoder')
+  //console.log(event.queryStringParameters)
 
-  /* parse the string body into a useable JS object */
-  const data = JSON.parse(event.body)
-  console.log(data)
-  console.log(Object.keys(data.payload));
-  console.log(Object.keys(data.site));
+  const params = querystring.parse(event);
+  console.log(params)
 
-  const commit = data.payload.commit_ref;
-  const branch = data.payload.branch;
+  var eventStr = JSON.stringify(event);
+  console.log(eventStr)
+  //var contextStr = JSON.stringify(context);
+  //console.log('context' + contextStr)
 
-  // do your stuff with variables...
-  //...
+  console.log('name: ' + event.queryStringParameters.name)
+  console.log('phone: ' + event.queryStringParameters.phone)
+  console.log('message: ' + event.queryStringParameters.message)
 
-}
+      request.post(
+          'https://' + process.env.TWILIO_SID + ':' + process.env.TWILIO_TOKEN + '@api.twilio.com/2010-04-01/Accounts/' + process.env.SID + '/Messages.json',
+          { json: {
+            "body": 'McAvoy or Stewart? These timelines can get so confusing.',
+            "from": '+18448231846',
+            "statusCallback": 'http://postb.in/1234abcd',
+            "to": '+34634315274'
+           } },
+          function (error, response, body) {
+              if (!error && response.statusCode >= 200) {
+                  console.log("sms looks good")
+                  console.log('body: ' + JSON.stringify(body))
+                  console.log('response: ' + JSON.stringify(response))
+              } else {
+                  console.log('error: ' + JSON.stringify(error))
+                  console.log('response error: ' + JSON.stringify(response))
+                  console.log('body error: ' + JSON.stringify(body))
+              }
+          }
+      );
+
+  return {
+    statusCode: 200,
+    body: "Hello, World"
+  };
+};
