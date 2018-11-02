579aa2745cbe9617da14f8b8e1a50e2bc20752e4
Ernesto Guimerans <ernestog@propertyvista.com>
2018-11-01 21:46:50 -0400

    cc

--- submission-created.js
+++ submission-created.js
@@ -1,42 +1,48 @@
-exports.handler = async (event, context) => {
+
+import querystring from "querystring";
+
+exports.handler = function(event, context, callback) {
   console.log('hoder')
-  //console.log(event.queryStringParameters)
-
-  const params = querystring.parse(event);
-  console.log(params)
-
-  var eventStr = JSON.stringify(event);
-  console.log(eventStr)
-  //var contextStr = JSON.stringify(context);
-  //console.log('context' + contextStr)
 
   console.log('name: ' + event.queryStringParameters.name)
   console.log('phone: ' + event.queryStringParameters.phone)
   console.log('message: ' + event.queryStringParameters.message)
 
-      request.post(
-          'https://' + process.env.TWILIO_SID + ':' + process.env.TWILIO_TOKEN + '@api.twilio.com/2010-04-01/Accounts/' + process.env.SID + '/Messages.json',
-          { json: {
-            "body": 'McAvoy or Stewart? These timelines can get so confusing.',
-            "from": '+18448231846',
-            "statusCallback": 'http://postb.in/1234abcd',
-            "to": '+34634315274'
-           } },
-          function (error, response, body) {
-              if (!error && response.statusCode >= 200) {
-                  console.log("sms looks good")
-                  console.log('body: ' + JSON.stringify(body))
-                  console.log('response: ' + JSON.stringify(response))
-              } else {
-                  console.log('error: ' + JSON.stringify(error))
-                  console.log('response error: ' + JSON.stringify(response))
-                  console.log('body error: ' + JSON.stringify(body))
-              }
-          }
-      );
-
-  return {
-    statusCode: 200,
-    body: "Hello, World"
+  var form = {
+    Body: 'one text message from netlify...',
+    From: '+18448231846',
+    To: '+34634315274',
+    StatusCallback: 'http://www.google.es'
   };
+
+  var formData = querystring.stringify(form);
+  var contentLength = formData.length;
+
+  var request = require('request');
+
+  request({
+    headers: {
+      'Content-Length': contentLength,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    uri: 'https://' + process.env.TWILIO_SID + ':' + process.env.TWILIO_TOKEN + '@api.twilio.com/2010-04-01/Accounts/' + process.env.TWILIO_SID + '/Messages.json',
+    body: formData,
+    method: 'POST'
+  }, function (error, response, body) {
+    if (!error && response.statusCode >= 200 && response.statusCode < 300) {
+        console.log("sms looks good")
+        console.log('body: ' + JSON.stringify(body))
+        console.log('response: ' + JSON.stringify(response))
+    } else {
+        console.log('error: ' + JSON.stringify(error))
+        console.log('response error: ' + JSON.stringify(response))
+        console.log('body error: ' + JSON.stringify(body))
+    }
+  });
+
+  callback(null, {
+    statusCode: 200,
+    body: "Everything looks fine :)"
+  });
+
 };
